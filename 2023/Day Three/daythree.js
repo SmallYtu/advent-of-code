508bc43cd61fd275e49ebc0d2a85ba54ec6ab264
SmallYtu <73671070+SmallYtu@users.noreply.github.com>
2023-12-03 16:20:08 -0500

    day three part 2
    asdasdasdasdasda

--- 2023/Day Three/daythree.js
+++ 2023/Day Three/daythree.js
@@ -1,11 +1,13 @@
-function solve(input) {
+const { readFileSync } = require("fs");
+const whar = readFileSync("Day Three/input.txt").toString();
+
+ function solve(input, part = 1) {
     input = input.split(/\n/)
-    console.log(input)
     let currentNum = 0
     let solution = 0
     let hasSkipped = 0
     let symbol = false
-
+    let cordData = {}
     for(line = 0; line < input.length; line++) {
 
         for(char = 0; char < input[line].length; char++) {
@@ -31,9 +33,18 @@ function solve(input) {
                 //seperating that shit code from this shit code
                 function isSymbol(x, y) {
                     if (isNaN(input[line + y].charAt(char-hasSkipped + x)) && input[line + y].charAt(char-hasSkipped + x) != ".") {
-                        solution += Number(currentNum)
-                        console.log(solution)
-                        console.log(currentNum)
+                        if (part === 1) {
+                            solution += Number(currentNum)
+                        } else { // THIS IS FOR PART 2 OF THE THING
+                            let symbolCord = `${x - hasSkipped + char}` + "y " + `${y + line}`
+                            if (!cordData[symbolCord]) {
+                                cordData[symbolCord] = currentNum
+                            } else {
+                                solution += cordData[symbolCord] * currentNum
+                            }
+                            
+                        }
+                        
                     }
                 }
                 if (hasSkipped == 2) {
@@ -64,5 +75,9 @@ function solve(input) {
         }
 
     }
-    return solution
+    console.log(solution)
 }
+console.log("PART 1")
+solve(whar, 1)
+console.log("PART 2")
+solve(whar, 2)
